3bf0f65489dcf2b87629954224ef4d6d52048be1
souzamaicon25 <97673422+souzamaicon25@users.noreply.github.com>
2022-01-28 12:46:32 -0300

    Repositório do curso de lógica com JavaScript

---
+++ Modulo_Logica/script.js
@@ -0,0 +1,11 @@
+let nome=prompt("Digite seu nome:")
+alert (`Olá ${nome}, seja bem vindo ao curso de imersão Java`)
+document.getElementById("saida").innerHTML=`Olá ${nome}, seja bem vindo ao curso de imersão Java`
+
+
+
+
+
+
+//document.getElementById("saida").innerHTML="Olá Mundo!"
+//alert('Olá Mundo')
